52dfa75199f6adac146ccaada5e4fc1158b9fe9c
MdAshique <asikhosen865575@gmail.com>
2023-12-26 16:08:36 +0600

    show display

--- js/app.js
+++ js/app.js
@@ -5,7 +5,30 @@ const searchPhone = ()=>{
     const url= `https://openapi.programming-hero.com/api/phones?search=${searchPhone}`
     fetch(url)
     .then(res => res.json())
-    .then(json => console.log(json.data))
+    .then(json => searchDisplay(json.data))
 
 }
 
+const searchDisplay = phones =>{
+   const serchPhones = document.getElementById('show-Phone');
+   phones.forEach(phone => {
+    console.log(phone);
+    const div= document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML=`
+    <div class="card">
+            <img  src="${phone.image}" class="card-img-top w-75" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${phone.phone_name}</h5>
+              <p class="card-text">A mobile phone (or cellphone) is a portable telephone that can make and receive calls over a radio frequency link while the user is moving within a telephone service area, as opposed to a fixed-location phone (landline phone).</p>
+              <button  class="btn btn-primary w-50">Show-Details</button>
+            </div>
+           
+          </div>
+          
+    `
+    serchPhones.appendChild(div);
+    
+   });
+
+}
