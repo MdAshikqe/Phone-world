6a21c053ca61e3ea1283de08d1c85eee6f554844
MdAshique <asikhosen865575@gmail.com>
2023-12-26 15:20:34 +0600

    search field

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('js Connected')
+const searchPhone = ()=>{
+    const phone= document.getElementById('phoneField');
+    const searchPhone= phone.value;
+    phone.value="";
+    const url= `https://openapi.programming-hero.com/api/phones?search=${searchPhone}`
+    fetch(url)
+    .then(res => res.json())
+    .then(json => console.log(json.data))
+
+}
+
