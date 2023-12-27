c6a79862f2387a0ce013e0876bc4227d60f179a2
MdAshique <asikhosen865575@gmail.com>
2023-12-27 13:39:29 +0600

    show single details

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// input search
 const searchPhone = ()=>{
     const phone= document.getElementById('phoneField');
     const searchPhone= phone.value;
@@ -8,11 +9,10 @@ const searchPhone = ()=>{
     .then(json => searchDisplay(json.data))
 
 }
-
+// show Phone 
 const searchDisplay = phones =>{
    const serchPhones = document.getElementById('show-Phone');
    phones.forEach(phone => {
-    console.log(phone);
     const div= document.createElement('div');
     div.classList.add('col');
     div.innerHTML=`
@@ -21,7 +21,7 @@ const searchDisplay = phones =>{
             <div class="card-body">
               <h5 class="card-title">${phone.phone_name}</h5>
               <p class="card-text">A mobile phone (or cellphone) is a portable telephone that can make and receive calls over a radio frequency link while the user is moving within a telephone service area, as opposed to a fixed-location phone (landline phone).</p>
-              <button  class="btn btn-primary w-50">Show-Details</button>
+              <button onclick="getPhone('${phone.slug}')"  class="btn btn-primary w-50">Show-Details</button>
             </div>
            
           </div>
@@ -32,3 +32,40 @@ const searchDisplay = phones =>{
    });
 
 }
+
+// show phone Details
+const getPhone=phoneId=>{
+const url= `https://openapi.programming-hero.com/api/phone/${phoneId}`;
+fetch(url)
+.then(res => res.json())
+.then(json => showDetails(json.data));
+}
+
+const showDetails=data=>{
+    console.log(data);
+    const manufacring= data.mainFeatures;
+    const getSinglePhone=document.getElementById('singlePhone');
+    const div=document.createElement('div');
+    div.innerHTML=`
+    <div class="card mb-3 mx-auto" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${data.image}" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${data.name}</h5>
+              <p class="card-text">Release Date: ${data.releaseDate}</p>
+              <p class="card-text">Phone Storage: ${manufacring.storage}</p>
+              <p class="card-text">Memory: ${manufacring.memory}</p>
+              <p class="card-text">Display-Size: ${manufacring.displaySize}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+    getSinglePhone.appendChild(div);
+    
+
+}
+
